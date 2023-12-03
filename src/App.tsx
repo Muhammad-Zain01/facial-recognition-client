4812d30f2ed46e65fd623bd12e28926f8dafeb2c
Muhammad-Zain01 <zainmemon010@gmail.com>
2023-12-03 17:39:01 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import React from "react"
+
 function App() {
   return (
     <>
-      <div>Project</div>
+      <div style={{display: 'flex'}}>
+        <div style={{width: '40%'}}>Register Face</div>
+        <div style={{width: '40%'}}>Check Face</div>
+      </div>
     </>
   )
 }
