6a2c27b06994e1559d7fda1fea67a5251de40682
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-04 17:56:10 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import './index.css'
-
+import { WebcamProvider } from './context/webcam.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ConfigProvider theme={{ token: { colorPrimary: '#0766AD', borderRadius: 4 } }}>
-        <App />
+        <WebcamProvider>
+          <App />
+        </WebcamProvider>
       </ConfigProvider>
     </BrowserRouter>
   </React.StrictMode>,
