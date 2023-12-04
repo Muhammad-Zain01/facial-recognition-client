402e6cda2e33a5ec486e4620c57023e3fe294854
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-04 10:34:32 +0500

    Update ui-box.tsx

--- src/components/ui-box/ui-box.tsx
+++ src/components/ui-box/ui-box.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { UIBoxWrapper, UIBoxImage, UIBoxChildren } from "./ui-box.style";
+import { useNavigate } from "react-router-dom";
 type ComponentProps = {
-    children: React.ReactNode
+    children: React.ReactNode;
+    href: String;
 }
-const UIBox: React.FC<ComponentProps> = ({ children }): JSX.Element => {
+const UIBox: React.FC<ComponentProps> = ({ href, children }): JSX.Element => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate(`/${href}`)
+    }
     return (
-        <UIBoxWrapper>
+        <UIBoxWrapper onClick={handleClick}>
             <UIBoxImage src="/face.jpg" alt="face" />
             <UIBoxChildren>{children}</UIBoxChildren>
         </UIBoxWrapper>
