aef247d52225c69ecca0fe6cace986acaae3bb45
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-04 10:34:28 +0500

    Update ui-box.style.tsx

--- src/components/ui-box/ui-box.style.tsx
+++ src/components/ui-box/ui-box.style.tsx
@@ -6,11 +6,18 @@ export const UIBoxWrapper = styled.div`
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    border: 1px solid #46464624;
+    border-radius: 0.5rem;
+    padding: 40px 20px;
+    transition: 0.2s ease-in-out;
+    &:hover{
+        border-color: #68686889;
+    }
 `
 export const UIBoxImage = styled.img`
     width: 70%;
 `
 export const UIBoxChildren = styled.div`
     font-size: 30px;
-    
+    font-weight: 600;
 `
