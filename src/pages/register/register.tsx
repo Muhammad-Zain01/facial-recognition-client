f789f80170fbfa74620c5e972c4eb946754ae517
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-04 17:56:14 +0500

    Update register.tsx

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,18 +1,41 @@
 import Uploader from "../../components/uploader/uploader";
-import { Typography, Input, Button } from "antd";
+import { Modal, UploadFile, Button } from "antd";
 import { RegisterHeading, RegisterLabel, RegisterWrapper, RegisterInput, RegisterButton } from "./register.style";
-
+import FaceCam from "../../components/facecam/facecam";
+import { useRef, useState } from "react";
+import { CameraOutlined } from "@ant-design/icons";
+import { useWebcamContext } from "../../hooks/useWebcam";
 const Register = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { setWebcamStarted, isDetected } = useWebcamContext();
+
+    const [fileList, setFileList] = useState<UploadFile[]>([
+        {
+            uid: '-2',
+            name: 'image.png',
+            status: 'done',
+            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        }
+    ]);
+    const captureImage = () => {
+
+    }
+    const handleModalOpen = () => {
+        setWebcamStarted(true);
+        setIsModalOpen(true);
+    }
     return (
         <RegisterWrapper>
+
             <RegisterHeading>
                 Register your face
             </RegisterHeading>
             <div>
                 <RegisterLabel>
-                    Upload Images <span>( Max 10 )</span>
+                    <div>Upload Images <span className="text">( Max 10 )</span></div>
+                    <div ><Button icon={<CameraOutlined />} onClick={handleModalOpen}>Capture</Button></div>
                 </RegisterLabel>
-                <Uploader />
+                <Uploader fileList={fileList} setFileList={setFileList} />
             </div>
             <div>
                 <RegisterLabel>
@@ -21,6 +44,27 @@ const Register = () => {
                 <RegisterInput placeholder="Muhammad Zain" />
                 <RegisterButton type="primary">Register</RegisterButton>
             </div>
+
+
+            <Modal
+                title="Add Image"
+                open={isModalOpen}
+                centered
+                width={690}
+                onCancel={() => {
+                    setWebcamStarted(false)
+                    setIsModalOpen(false)
+                }}
+                footer={
+                    <div>
+                        <Button type="primary" disabled={!isDetected} onClick={captureImage}>Capture</Button>
+                    </div>
+                }
+            >
+                <div style={{ height: 500, margin: 'auto' }}>
+                    <FaceCam key={IsWeb} />
+                </div>
+            </Modal>
         </RegisterWrapper>
     )
 }
