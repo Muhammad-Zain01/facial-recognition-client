1020e3d7ae06646677a2584fe41fa2e4cdd2f411
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-04 09:54:07 +0500

    Create ui-box.style.tsx

---
+++ src/components/ui-box/ui-box.style.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const UIBoxWrapper = styled.div`
+    width: 33%;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+export const UIBoxImage = styled.img`
+    width: 70%;
+`
+export const UIBoxChildren = styled.div`
+    font-size: 30px;
+    
+`
