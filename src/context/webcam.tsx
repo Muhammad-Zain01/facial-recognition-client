a40a65727df9e6fa05ee069bd74e9c9323157c16
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-05 09:43:31 +0500

    Update webcam.tsx

--- src/context/webcam.tsx
+++ src/context/webcam.tsx
@@ -1,19 +1,23 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useRef } from "react";
 
 type ContextState = {
-    isDetected: Boolean
-    WebcamStarted: Boolean
+    isDetected: Boolean;
+    WebcamStarted: Boolean;
+    WebCamRef: any;
     setIsDetected?: (value: Boolean) => void
     setWebcamStarted?: (value: Boolean) => void
+    setWebCamRef?: (value: any) => void
 }
 const initialState: ContextState = {
     isDetected: false,
     WebcamStarted: false,
+    WebCamRef: false
 }
 const defaultValues: ContextState = {
     ...initialState,
     setIsDetected: (value: Boolean) => { },
     setWebcamStarted: (value: Boolean) => { },
+    setWebCamRef: (value: any) => { },
 }
 export const WebCamContext = createContext(defaultValues);
 const WebcamReducer = (state, action) => {
@@ -28,6 +32,11 @@ const WebcamReducer = (state, action) => {
                 ...state,
                 WebcamStarted: action.payload
             }
+        case "SET_WEBCAM_REF":
+            return {
+                ...state,
+                WebCamRef: action.payload
+            }
     }
 }
 type Props = {
@@ -37,7 +46,8 @@ export const WebcamProvider: React.FC<Props> = ({ children }) => {
     const [state, dispatch] = useReducer(WebcamReducer, initialState)
     const setIsDetected = (value: Boolean): void => dispatch({ type: "SET_DETECTED", payload: value })
     const setWebcamStarted = (value: Boolean): void => dispatch({ type: "SET_WEBCAM", payload: value })
-    const value = { ...state, setIsDetected, setWebcamStarted }
+    const setWebCamRef = (ref: any): void => dispatch({ type: "SET_WEBCAM_REF", payload: ref })
+    const value = { ...state, setIsDetected, setWebcamStarted, setWebCamRef }
     return (
         <WebCamContext.Provider value={value}>
             {children}
