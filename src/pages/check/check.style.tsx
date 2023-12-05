1d62ad84e9c680b34d8f0ff12c012d98b8f7315d
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-05 11:09:26 +0500

    Create check.style.tsx

---
+++ src/pages/check/check.style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const CheckWrapper = styled.div`
+    margin: auto;
+    margin-top: 100px;
+    width: 640px;
+    justify-content: center;
+    height: 480px;
+`
