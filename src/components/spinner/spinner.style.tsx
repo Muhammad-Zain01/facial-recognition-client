ff83a40e1aa847a9a6faaba2994e2beb86b79c53
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-05 11:09:22 +0500

    Create spinner.style.tsx

---
+++ src/components/spinner/spinner.style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SpinnerWrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
