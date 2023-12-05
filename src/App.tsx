37958814ba79c1b65df3edc82ee71ea25fd1e991
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-05 11:09:18 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { Routes, Route } from "react-router-dom"
-import Home from "./pages/home/home"
-import Register from "./pages/register/register"
+const Home = React.lazy(() => import('./pages/home/home'));
+const Register = React.lazy(() => import('./pages/register/register'));
+const Check = React.lazy(() => import('./pages/check/check'));
+import Spinner from "./components/spinner/spinner";
 function App(): JSX.Element {
   return (
     <>
-      <Routes>
-        <Route path='/register' element={<Register />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path='/register' element={<Register />} />
+          <Route path='/check' element={<Check />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Suspense>
     </>
   )
 }
