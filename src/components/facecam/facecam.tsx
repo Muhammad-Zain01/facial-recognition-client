409b86236fdb40e47a0134c1a7770ecc51c58758
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-05 09:43:24 +0500

    Update facecam.tsx

--- src/components/facecam/facecam.tsx
+++ src/components/facecam/facecam.tsx
@@ -7,67 +7,51 @@ const FaceCam: React.FC = () => {
   const webcamRef = useRef<any>(null);
   const canvasRef = useRef<any>(null);
   const intervalId = useRef<any>(null);
-  const { WebcamStarted, setIsDetected } = useWebcamContext();
+  const { WebcamStarted, setIsDetected, setWebCamRef } = useWebcamContext();
   const loadModels = async () => {
     const MODEL_URL = 'https://justadudewhohacks.github.io/face-api.js/models';
     await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
   };
   const handleWebcamStream = async () => {
-    console.log("STARTED ==> ", webcamRef.current.video.readyState)
     if (webcamRef.current && webcamRef.current.video.readyState === 4) {
+      setWebCamRef(webcamRef.current)
       const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
-
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
-
       startFaceDetection(video, videoWidth, videoHeight)
-
     }
   };
   const startFaceDetection = (video, videoWidth, videoHeight) => {
     intervalId.current = setInterval(async () => {
       const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
-      if (detections.length > 0) { setIsDetected(true) }
+      if (detections.length > 0) { setIsDetected(true); }
       else { setIsDetected(false) }
       const resizedDetections = faceapi.resizeResults(detections, { width: videoWidth, height: videoHeight });
       canvasRef.current.getContext('2d').clearRect(0, 0, videoWidth, videoHeight);
       faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
     }, 100);
   };
-  const captureImage = () => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    console.log(imageSrc);
-  };
+  
 
   const stopFaceDetection = () => {
     if (intervalId.current) {
       clearInterval(intervalId.current);
     }
   };
-  const stopWebcam = () => {
-    if (webcamRef.current && webcamRef.current.video) {
-      const stream = webcamRef.current.video.srcObject;
-      const tracks = stream.getTracks();
-      tracks.forEach(track => track.stop());
-    }
-  };
 
   useEffect(() => {
     if (!WebcamStarted) {
       stopFaceDetection();
-      stopWebcam();
+    } else {
+      handleWebcamStream();
     }
   }, [WebcamStarted])
   useEffect(() => {
-    loadModels().then(() => {
-      if (WebcamStarted) {
-        handleWebcamStream();
-      }
-    })
+    loadModels()
   }, []);
 
   return (
