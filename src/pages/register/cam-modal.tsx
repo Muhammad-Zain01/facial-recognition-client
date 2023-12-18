98fad678e5776c063674aa8c3c4df3e7f9a216b7
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-18 12:04:27 +0500

    Update cam-modal.tsx

--- src/pages/register/cam-modal.tsx
+++ src/pages/register/cam-modal.tsx
@@ -5,21 +5,27 @@ import { SpinWrapper, ModalContent } from "./register.style";
 type ComponentProps = {
     isModalOpen: boolean;
     handleModalClose: () => void;
-    captureImage: () => void
+    captureImage: () => void;
+    title: string;
+    buttonText: string;
+    extra: boolean | React.ReactNode;
 }
-const CamModal: React.FC<ComponentProps> = ({ isModalOpen, handleModalClose, captureImage }): JSX.Element => {
+const CamModal: React.FC<ComponentProps> = ({ title = "Add Image", buttonText = "Capture", isModalOpen, handleModalClose, captureImage, extra = false }): JSX.Element => {
     const { resolution, isDetected, WebCamRef } = useWebcamContext()
     const { width, height } = resolution;
     return (
         <Modal
-            title="Add Image"
+            title={title}
             open={isModalOpen}
             centered
             width={`calc(${width}px + 45px)`}
             onCancel={handleModalClose}
             footer={
-                <div>
-                    <Button type="primary" disabled={!isDetected} onClick={captureImage}>Capture</Button>
+                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                    <div>
+                        {extra}
+                    </div>
+                    <Button type="primary" disabled={!isDetected} onClick={captureImage}>{buttonText}</Button>
                 </div>
             }
         >
