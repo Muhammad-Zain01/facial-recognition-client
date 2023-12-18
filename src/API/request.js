e4e112672179e85ee7406bda5bdd3e934957bd31
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-18 12:04:17 +0500

    Update request.js

--- src/API/request.js
+++ src/API/request.js
@@ -17,10 +17,27 @@ export const UploadImage = async (data) => {
         .catch((error) => { console.log(error) });
 }
 export const RegisterUser = async (path_id) => {
-    let url = `${import.meta.env.VITE_SERVER_URL}/upload`;
+    let url = `${import.meta.env.VITE_SERVER_URL}/register`;
 
     const body = new FormData();
     body.append('path', path_id);
+    let config = {
+        method: "POST",
+        maxBodyLength: Infinity,
+        url: url,
+        data: body
+    };
+
+    return await axios.request(config)
+        .then((response) => response.data)
+        .catch((error) => { console.log(error) });
+}
+
+export const CheckUser = async (image) => {
+    let url = `${import.meta.env.VITE_SERVER_URL}/check`;
+
+    const body = new FormData();
+    body.append('img', image);
     let config = {
         method: "POST",
         maxBodyLength: Infinity,
