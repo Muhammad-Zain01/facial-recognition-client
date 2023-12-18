6b9c8922d7eb8e2d115a66dded43dca2700e0caa
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-18 12:04:32 +0500

    Update register.tsx

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,22 +1,23 @@
 import { UploadFile, Button, message } from "antd";
 import { RegisterHeading, RegisterLabel, RegisterWrapper, RegisterInput, RegisterButton } from "./register.style";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useWebcamContext } from "../../hooks/useWebcam";
 import CamModal from "./cam-modal";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import { useNavigate } from 'react-router-dom';
 import UploadImages from "./upload-images";
 import RegisterModal from "./register-modal";
-import { UploadImage } from '../../API/request'
+import { UploadImage, RegisterUser } from '../../API/request'
 const Register = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [registerModal, setRegisterModal] = useState(true);
-    const [registerState, setRegisterState] = useState(3)
+    const [registerModal, setRegisterModal] = useState(false);
+    const [registerState, setRegisterState] = useState(0)
     const [percent, setPercent] = useState(0)
     const NameRef = useRef<any>("");
     const { setWebcamStarted, WebCamRef } = useWebcamContext();
     const [fileList, setFileList] = useState<UploadFile[]>([]);
     const navigate = useNavigate()
+
     const captureImage = () => {
         const imageSrc = WebCamRef.getScreenshot();
         const uuid = crypto.randomUUID();
@@ -39,31 +40,49 @@ const Register = () => {
         setWebcamStarted(false)
         setIsModalOpen(false)
     }
+    const Reset = () => {
+        setRegisterModal(false)
+        setRegisterState(0)
+        setPercent(0)
+        setFileList([])
+    }
     const onRegister = () => {
         const name = NameRef.current.input.value;
         if (name == '') {
             return message.error('Please enter your name');
         }
+        if (fileList.length == 0) {
+            return message.error('Please Upload your Images');
+        }
 
         setRegisterModal(true)
-        const id = crypto.randomUUID();
         const length = fileList.length
         fileList.map(async (item, index) => {
             const img = item?.url
-            const response = await UploadImage({ id, name, img })
+            const response = await UploadImage({ id: name, name, img })
             if (response?.Status) {
                 setPercent(Math.round(((index + 1) / length) * 100))
             } else {
                 message.error('Error Uploading')
                 setRegisterState(2);
             }
-            if (percent == 100) {
-                setRegisterState(1);
-            }
         })
 
     }
-
+    const startRegister = async () => {
+        setRegisterState(4)
+        const name = NameRef.current.input.value;
+        const response = await RegisterUser(name)
+        if (response?.Status) {
+            return setRegisterState(3)
+        }
+        return setRegisterState(2)
+    }
+    useEffect(() => {
+        if (percent == 100) {
+            setRegisterState(1);
+        }
+    }, [percent])
     return (
         <RegisterWrapper>
             <Button icon={<ArrowLeftOutlined />} onClick={() => navigate(-1)}>Back</Button>
@@ -79,7 +98,7 @@ const Register = () => {
                 <RegisterButton type="primary" onClick={onRegister}>Register</RegisterButton>
             </div>
             <CamModal isModalOpen={isModalOpen} handleModalClose={handleModalClose} captureImage={captureImage} />
-            <RegisterModal modalOpen={registerModal} setModalOpen={setRegisterModal} state={registerState} percent={percent} />
+            <RegisterModal modalOpen={registerModal} reset={Reset} state={registerState} percent={percent} startRegister={startRegister} />
         </RegisterWrapper>
     )
 }
