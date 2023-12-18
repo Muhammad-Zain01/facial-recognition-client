899b0d4b740f9cd3727ef33904dda5f8455fe4d4
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-12-18 13:03:11 +0500

    Update home.style.tsx

--- src/pages/home/home.style.tsx
+++ src/pages/home/home.style.tsx
@@ -7,6 +7,9 @@ export const HomePageContainer = styled.div`
     height: 700px;
     flex-direction: column;
     align-items: center;
+    @media only screen and (max-width: 805px) {
+        height: unset;
+    }
 `
 export const HomePageHeadingContainer = styled.div`
     margin-bottom: 30px;
@@ -14,9 +17,16 @@ export const HomePageHeadingContainer = styled.div`
 export const HomePageHeading = styled(Typography.Title)`
     font-weight: 800;
     font-size: 50px !important;
+    margin-top: 100px;
+    @media only screen and (max-width: 805px) {
+        font-size: 30px !important;
+    }
 `
 export const HomePageBox = styled.div`
     display: flex;
     justify-content: space-evenly;
     align-items: center;
+    @media only screen and (max-width: 805px) {
+        flex-direction: column;
+    }
 `
